5a7986a0b675aa7b376113f25f9e2d41dd0b8f65
João Guilherme <joaobenine@gmail.com>
2023-05-23 18:26:06 -0300

    teste api input

--- my-app/src/app/login/page.jsx
+++ my-app/src/app/login/page.jsx
@@ -1,17 +1,43 @@
 'use client'
 
 import { useState } from "react"
-
 export default function Login() {
-  const [keyApi, setKeyApi] = useState(null)
+  const [keyApi, setKeyApi] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    var myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-key", keyApi);
+    myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      // redirect: 'follow'
+    };
+
+    fetch("https://v3.football.api-sports.io/status", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    console.log({
+      keyApi
+    })
+  }
 
   return (
     <section className="flex-1 h-screen bg-zinc-800">
-      <div className="flex flex-col gap-2">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
         <label htmlFor="keyAccess" className="text-sm">Insira key para acesso:</label>
-        <input type="text" name="keyAccess" id="keyAccess" className="w-60 bg-zinc-400 rounded-sm focus:outline-none px-2 py-0.5 text-zinc-900" value={keyApi} />
+        <input
+          className="w-[280px] bg-zinc-400 rounded-sm focus:outline-none px-2 py-0.5 text-zinc-900"
+          type="text"
+          name="keyAccess"
+          id="keyAccess"
+          value={keyApi}
+          onChange={({ target }) => setKeyApi(target.value)} />
         <button type="submit" className="bg-green-600 w-24 mt-1 rounded-sm hover:bg-green-500">Enviar</button>
-      </div>
+      </form>
 
     </section>
   )
