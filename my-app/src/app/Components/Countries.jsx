b44f2ef1e872b3e11e7800161c2db338ca3b0af0
João Guilherme <joaobenine@gmail.com>
2023-05-24 23:58:49 -0300

    recebendo id select countries

--- my-app/src/app/Components/Countries.jsx
+++ my-app/src/app/Components/Countries.jsx
@@ -3,11 +3,12 @@ import fetchDefault from '../axios/axiosConfig'
 
 function Countries() {
   const [dataCountries, setDataCountries] = useState([])
+  const [valueSelect, setValueSelect] = useState('')
 
   useEffect(() => {
     async function FetchCountries() {
       try {
-        const fetchResponse = await fetchDefault('https://v1.basketball.api-sports.io/countries', {
+        const fetchResponse = await fetchDefault('/countries', {
           headers: {
             "x-rapidapi-key": "a3663050d10164fbaa3d5c0e5ff68f3a",
             "x-rapidapi-host": "v1.basketball.api-sports.io"
@@ -22,12 +23,18 @@ function Countries() {
     FetchCountries()
   }, [])
 
+
+  function handleChange(event) {
+    console.log(event.target.value)
+    setValueSelect(event.target.value);
+  }
+
   return (
     <div className="flex flex-col gap-2">
       <label className="text-sm">Selecione um Páis:</label>
-      <select className="w-[280px] bg-zinc-400 rounded-sm focus:outline-none px-2 py-0.5 text-zinc-900">
+      <select value={valueSelect} onChange={handleChange} className="w-[280px] bg-zinc-400 rounded-sm focus:outline-none px-2 py-0.5 text-zinc-900">
         {dataCountries.map(item => (
-          <option key={item.id}>{item.name}</option>
+          <option key={item.id} value={item.id} >{item.name}</option>
         ))}
       </select>
     </div>
