ad13e067e40e4a3d2706fd560d70eeb969998737
João Guilherme <joaobenine@gmail.com>
2023-05-26 14:11:51 -0300

    função ligas assyncronas

--- my-app/src/app/Components/Countries.jsx
+++ my-app/src/app/Components/Countries.jsx
@@ -21,43 +21,30 @@ function Countries() {
     FetchCountries()
   }, [])
 
-  function handleCountry(event) {
-    console.log(event.target.value)
-    setValueCountry(event.target.value);
-    console.log(`value contry:${valueCountry}`)
-
-
-    async function FetchLeagues() {
-      fetchDefault(`/leagues?country_id=${valueCountry}`)
-      .then(function (responseLeague) {
-        setDataLeagues(responseLeague.data.response)
+  async function handleCountry(event) {
+    const selectedCountry = event.target.value;
+    console.log(selectedCountry);
+    setValueCountry(selectedCountry);
+  
+    async function FetchLeagues(countryId) {
+      try {
+        const responseLeague = await fetchDefault(`/leagues?country_id=${countryId}`);
+        setDataLeagues(responseLeague.data.response);
         console.log(responseLeague.data.response);
-      })
-      .catch(function(error) {
-        // manipula erros da requisição
+      } catch (error) {
+        // Manipule erros da requisição
         console.error(error);
-      })
+      }
     }
-
-  //  async  function antiga(){
-  //     try {
-  //       const fetchLeagueData = await fetchDefault.get(`/leagues`)
-  //       const dataFetchLeagues = await fetchLeagueData.data.response;
-  //       console.log(dataFetchLeagues)
-  //       setDataLeagues(dataFetchLeagues)
-        
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
-  //   }
-    FetchLeagues()
+  
+    if (selectedCountry !== null) {
+      FetchLeagues(selectedCountry);
+    }
   }
 
-  function handleLeague(event){
-    console.log('action function')
-    console.log(event.target.value)
-    // setValueLeague(event.target.value)
-    // handleLeague();
+  function handleLeague(event) {
+    console.log('evento league')
+    setValueLeague(event.target.value)
   }
 
   return (
@@ -75,9 +62,9 @@ function Countries() {
         <div className="flex flex-col gap-1">
           <label className="text-sm">Selecione um Liga:</label>
           <select value={valueLeague} onChange={handleLeague} className="w-[280px] bg-zinc-400 rounded-sm focus:outline-none px-2 py-0.5 text-zinc-900">
-          {dataLeagues.map((league) => (
-            <option className="text-black" key={league.id} value={league.id}>{league.name}teste</option>
-          ))}
+            {dataLeagues.map((league) => (
+              <option className="text-black" key={league.id} value={league.id}>{league.name}teste</option>
+            ))}
           </select>
         </div>
       ) : <p></p>}
