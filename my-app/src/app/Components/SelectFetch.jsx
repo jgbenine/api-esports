1536976fc991013dbfed0f4db86ce1ebdde7d035
João Guilherme <joaobenine@gmail.com>
2023-05-29 12:39:54 -0300

    montando requisição fetch

--- my-app/src/app/Components/SelectFetch.jsx
+++ my-app/src/app/Components/SelectFetch.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import fetchDefault from "../axios/axiosConfig"
 
 
 
 function SelectFetch({url, mapFunction, value, onChange}) {
   const [options, setOptions] = useState([])
 
-  
+  useEffect(()=>{
+    async function fetchData(){
+      try{
+        const response = await fetchDefault(url);
+        const data = response.data;
+        const mappedOptions = data.map(mapFunction);
+        setOptions(mappedOptions)
+      }catch(error){
+        console.log(error)
+      }
+    }
+    fetchData();
+  },[url, mapFunction])
+
   return (
 
   )
