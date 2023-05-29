2a05395f060682b24d6ffeab4b0a235c21f6ad43
João Guilherme <joaobenine@gmail.com>
2023-05-29 13:05:04 -0300

    teste de selectFetch reeutilizavel usando a rota countries, funfando.

--- my-app/src/app/Components/SelectFetch.jsx
+++ my-app/src/app/Components/SelectFetch.jsx
@@ -10,9 +10,15 @@ function SelectFetch({url, mapFunction, value, onChange}) {
     async function fetchData(){
       try{
         const response = await fetchDefault(url);
-        const data = response.data;
-        const mappedOptions = data.map(mapFunction);
-        setOptions(mappedOptions)
+        const data = response.data.response;
+         // Verificar se data é um array
+         if (Array.isArray(data)) {
+          const mappedOptions = data.map(mapFunction);
+          setOptions(mappedOptions);
+          console.log(data)
+        } else {
+          console.error('Essa estrtura não é um array permitido map');
+        }
       }catch(error){
         console.log(error)
       }
@@ -21,8 +27,14 @@ function SelectFetch({url, mapFunction, value, onChange}) {
   },[url, mapFunction])
 
   return (
-
+    <select className="text-black" value={value} onChange={onChange}>
+      {options.map((option) =>(
+        <option key={option.id} value={option.id}>
+          {option.label}
+        </option>
+      ))}
+    </select>
   )
 }
 
-export default SelectFetch
+export default SelectFetch;
--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -2,29 +2,41 @@
 import { useState } from "react"
 import fetchDefault from "./axios/axiosConfig"
 import Countries from "./Components/Countries"
+import SelectFetch from "./Components/SelectFetch"
 
 export default function Home() {
-  const [inputKey, setInput] = useState('')
-  const [infoLogin, setInfoLogin] = useState([''])
+  // const [inputKey, setInput] = useState('')
+  // const [infoLogin, setInfoLogin] = useState([''])
+  const [selectedOption, setSelectedOption] = useState('');
 
-  async function handleSubmit(event) {
-    event.preventDefault()
-    try {
-      const fetchResponse = await fetchDefault('/status', {
-        headers: {
-          "x-rapidapi-key": inputKey,
-        }
-      })
-      const dataFetch = await fetchResponse.data.results;
-      setInfoLogin(dataFetch)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const mapFunction = (data) => ({
+    id: data.id,
+    label: data.name,
+  });
+
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+
+  // async function handleSubmit(event) {
+  //   event.preventDefault()
+  //   try {
+  //     const fetchResponse = await fetchDefault('/status', {
+  //       headers: {
+  //         "x-rapidapi-key": inputKey,
+  //       }
+  //     })
+  //     const dataFetch = await fetchResponse.data.results;
+  //     setInfoLogin(dataFetch)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
 
   return (
-    <section className="flex flex-1 h-screen bg-zinc-800 gap-3">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+    <section className="flex flex-col flex-1 h-screen bg-zinc-800 gap-3">
+      {/* <form onSubmit={handleSubmit} className="flex flex-col gap-2">
         <div className="flex flex-col gap-2">
           <label htmlFor="keyAccess" className="text-sm">Insira key para acesso:</label>
           <input
@@ -39,8 +51,20 @@ export default function Home() {
         </div>
         {infoLogin === 0 ? (<p className="text-[0.7rem] text-red-600">Key Inválido</p>) :
           infoLogin === 1 ? (<Countries />)
-          :(<p></p>)}
-      </form>
+            : (<p></p>)}
+      </form> */}
+
+      <div className="flex flex-col w-60">
+        <label>Selecione Pais</label>
+        <SelectFetch
+            url="/countries"
+            mapFunction={mapFunction}
+            value={selectedOption}
+            onChange={handleSelectChange}
+        />
+        <p>Option select: {selectedOption}</p>
+      </div >
     </section>
+
   )
 }
